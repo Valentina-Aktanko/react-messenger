ff45a2d142ced2b5b340458c0dde61cd489a5a95
Valentina A <valentina@tandem-m.de>
2021-02-18 10:38:47 +0300

    react router path exact

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,19 @@ import 'assets/global.css';
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// browser-routing must be configured on the webserver
+// for hash router must use hash-router
+// autoscaling on the services, such as amason. google claude, microsoft azure and so on
 
 import { Layout } from 'components/Layout';
 
 ReactDom.render(
-  <Layout />,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Layout} />
+      <Route exact path="/chats/:id" component={Layout} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
